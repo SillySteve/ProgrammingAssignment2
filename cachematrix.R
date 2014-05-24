908e5f6b84dcf004543112d09884dd26daacda6b
Steve Silveri <goofy.steve@gmail.com>
2014-05-24 08:02:56 -0700

    Cachematrix functions for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are a setup of functions to cache a matrix
+##  and then cache the inverse of the matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix 
+##    will take a matrix as an input and cache the 
+##    matrix if it does not already exist
+##    
+##    paramters
+##      x - matrix object
+##      returns the cache matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  ## function set x in the calling envronment
+  set <- function(y) {
+      x <<- y
+      cachem <<- y
+  }
+
+  
+  ## set x in the calling envronment
+  
+  set(x)
+  
+  ## check to see if we already have a cached matrix
+  ## and if it is indentical to the one we are passing now
+  ##  if it us new, set it
+  
+  if (!is.null(cachem)  && !identical(x,cachem)) (
+     cachem <<- x
+    
+  )
+  return(cachem)
 }
+solve(a)
 
+## cacheSolve 
+##    will take a matrix as an input and return the inverse 
+##    if wukk cache the result 
+##    
+##    paramters
+##      x - matrix object
+##      returns the cache matrix
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+
+  set <- function(y) {
+    x <<- y
+    cachex <<- y
+  }
+  
+  
+  ## set x in the calling envronment
+  
+  set(x)
+  
+  ## i the answer is already cache... just return it
+  if (!is.null(cachex)  || !identical(x,cachex)) {
+    
+    cachex <<- solve(x, ...)%*%x
+    
+  } 
+  
+  return(cachex)
+  
+  
+        
 }
